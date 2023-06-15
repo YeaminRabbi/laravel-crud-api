4bb9b07b533446637d69bb810e8cd58899a96b2a
yeaminrabbi <yeaminrabbi308@gmail.com>
2023-06-15 13:52:38 +0600

    Laravel API Crud | Products Module

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        $products = Product::orderBy('id', 'desc')->paginate(10);
+        return response()->json([
+            'products' => $products
+        ], 200);
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+        ]);
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->quantity = rand(15, 100);
+        $product->save();
+
+        
+        return response()->json([
+                'message' => 'Product created successfully',
+                'product' => $product
+        ], 201);
+           
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+            
+        return response()->json([
+            'product' => $product
+        ], 200);
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return response()->json([
+            'product' => $product
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+        ]);
+        
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->quantity = rand(15, 100);
+        $product->save();
+
+        return response()->json([
+            'message' => 'Product updated successfully',
+            'product' => $product
+        ], 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return [
+            'message' => 'Product deleted successfully'
+        ];
+    }
+}
